eeceec837459eab4ac15a317facea99619d0df28
nikhil0162 <nikhil0162@gmail.com>
2021-02-09 19:35:46 +0530

    initial

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer>
+            <h1>Footer</h1>
+            Copyright 2021 Nikhil
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+            <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
+        </div>
+    );
+}
+
+export default About;
